edf433e3a31de60c6344af521d4e81575d6b95da
Atul Bopche <atul962355@gmail.com>
2025-04-01 19:04:26 +0530

    Redux Store complete

--- blogAppwriteProject/src/App.jsx
+++ blogAppwriteProject/src/App.jsx
@@ -1,14 +1,29 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux';
+import {login, logout} from './store/authSlice'
+import authService from './appwrite/auth';
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  const [loding, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  )
+  useEffect(()=> {
+    authService.getCurrentUser()
+    .then((userData)=> {
+      if (userData) {
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=> setLoading(false))
+  }, [])
+
+ return !loding ? (
+  <div className='bg-gray-500'>Hello World</div>
+ ) : null
 }
 
 export default App
